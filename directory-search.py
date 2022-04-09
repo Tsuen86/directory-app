77da83a371be8e2132cd4b89ea9858bec3a66b3d
Tsuen86 <72797003+Tsuen86@users.noreply.github.com>
2022-04-09 17:42:19 +0800

    Update directory-search.py

--- directory-search.py
+++ directory-search.py
@@ -1,54 +1,54 @@
-# # Establish Search Directory Definitions
+# Establish Search Directory Definitions
 
-# # Node class
-# class Node:
-#     def __init__(self, dataValue):
-#         self.dataValue = dataValue
-#         self.nextNode = None
+# Node class
+class Node:
+    def __init__(self, dataValue):
+        self.dataValue = dataValue
+        self.nextNode = None
  
  
-# # Constructor to initialize the node object
-# class LinkedList:
+# Constructor to initialize the node object
+class LinkedList:
  
-#     # Function to initialize head
-#     def __init__(self):
-#         self.headNode = None
+    # Function to initialize head
+    def __init__(self):
+        self.headNode = None
  
-#     # Method to print linked list
-#     def printList(self):
-#         currentNode = self.headNode
+    # Method to print linked list
+    def printList(self):
+        currentNode = self.headNode
          
-#         while currentNode :
-#             print(currentNode.dataValue)
-#             currentNode = currentNode.nextNode
+        while currentNode :
+            print(currentNode.dataValue)
+            currentNode = currentNode.nextNode
  
-#     # Function to add of node at the end.
-#     def append (self, data):
-#         newNode = Node(data)
-#         if self.headNode is None:
-#             self.headNode = newNode
-#             return
-#         currentNode = self.headNode
-#         while currentNode.nextNode is not None:
-#             currentNode = currentNode.nextNode
-#         currentNode.nextNode = newNode
+    # Function to add of node at the end.
+    def append (self, data):
+        newNode = Node(data)
+        if self.headNode is None:
+            self.headNode = newNode
+            return
+        currentNode = self.headNode
+        while currentNode.nextNode is not None:
+            currentNode = currentNode.nextNode
+        currentNode.nextNode = newNode
         
         
-#     # Checks whether the element  
-#     # is present in linked list  
-#     def linearSearch(self, headNode,element): 
+    # Checks whether the element  
+    # is present in linked list  
+    def linearSearch(self, headNode,element): 
           
-#         # Base case 
-#         if headNode==None: 
-#             return False
+        # Base case 
+        if headNode==None: 
+            return False
           
-#         # If key is present in  
-#         # current node, return true 
-#         if(headNode.dataValue == element): 
-#             return True
+        # If key is present in  
+        # current node, return true 
+        if(headNode.dataValue == element): 
+            return True
           
-#         # Recur for remaining list 
-#         return self.linearSearch (headNode.nextNode, element) 
+        # Recur for remaining list 
+        return self.linearSearch (headNode.nextNode, element) 
       
 import streamlit as st 
 
@@ -58,39 +58,39 @@ This is a web app to add onto and search through a Directory.
 
 option = st.sidebar.selectbox(
      'Choose an Action:',
-     ('Add to Directory', 'Search Directory', 'Print Directory', 'Exit Directory'))
+     ('', 'Add to Directory', 'Search Directory', 'Print Directory', 'Exit Directory'))
 
 st.sidebar.write('You selected: ', option)
 
 ##########################################################
     
-# st.header("Add & Search: Live Directory\n\n")
+st.header("Add & Search: Live Directory\n\n")
 
-# directory = LinkedList()
+directory = LinkedList()
 
-# while True:
-#     print("\n\nYou selected: {}.\n".format(option))
-#     if option == 'Exit Directory':
-#         print("Thank you for using our service. ")
-#         break
-#     try:
-#         if option=='Add to Directory':
+while True:
+    print("\n\nYou selected: {}.\n".format(option))
+    if option == 'Exit Directory':
+        print("Thank you for using our service. ")
+        break
+    try:
+        if option=='Add to Directory':
           
-#             # Add data point to directory
-#             item1 = input("\nPlease enter item to add to Directory.\n\n")
-#             directory.append(item1)
-#             print("\nItem Added: {}.".format(item1))
+            # Add data point to directory
+            item1 = input("\nPlease enter item to add to Directory.\n\n")
+            directory.append(item1)
+            print("\nItem Added: {}.".format(item1))
 
-#         elif option=='Search Directory':
+        elif option=='Search Directory':
             
-#             item2 = input("\nPlease enter search item.\n\n")
-#             directory.linearSearch(directory.headNode,item2)
+            item2 = input("\nPlease enter search item.\n\n")
+            directory.linearSearch(directory.headNode,item2)
         
-#         else:
-#             directory.printList()
+        else:
+            directory.printList()
          
     
-#     except ValueError:
-#         print("\nPlease select an Action from the sidebar.\n\n")
+    except ValueError:
+        print("\nPlease select an Action from the sidebar.\n\n")
     
     
